a7a8cbe31c950dc12c492619c4c0da661b3f2dff
devLateef <lateefolatayo@gmail.com>
2025-05-11 07:55:05 +0100

    added sidebar

--- resources/js/Layouts/HomeLayout.jsx
+++ resources/js/Layouts/HomeLayout.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import SideBar from "../Pages/SideBar";
 
 const HomeLayout = ({ children }) => {
     return (
         <div className="min-h-screen flex flex-col">
             {/* You can add any special header or footer here for homepage */}
-            <header className="bg-blue-500 text-white p-4 text-center">
-                Home Page Header
-            </header>
-            <main className="flex-grow">{children}</main>
-            <footer className="bg-blue-500 text-white p-4 text-center">
+            <div className="flex min-h-screen bg-amber-50">
+                <header className="bg-gray-500 text-white p-4 text-center">
+                    <SideBar/>
+                </header>
+                <main className="flex-grow">{children}</main>
+            </div>
+            <footer className="bg-gray-500 text-white p-4 text-center">
                 Home Page Footer
             </footer>
         </div>
---
+++ resources/js/Pages/SideBar.jsx
@@ -0,0 +1,14 @@
+import HomeLayout from "../Layouts/HomeLayout";
+
+const SideBar = () => {
+    return(
+        <div>
+            <div className="flex gap-4 items-center py-2">
+                <h2 className="font-bold">Welcome Ade</h2>
+                <div className="h-10 w-10 bg-amber-100 rounded-full ring-2"></div>
+            </div>
+        </div>
+    )
+}
+
+export default SideBar;
